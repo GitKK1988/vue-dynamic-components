f9212ce7b7e4936c2d6f980198b9d741ded2fd35
GitKK1988 <info@kim-kolkmann.de>
2023-01-13 16:17:48 +0100

    20220113-01

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script>
+import DynComp1 from './components/DynComp1.vue'
+import DynComp2 from './components/DynComp2.vue'
+import DynComp3 from './components/DynComp3.vue'
+
+
+export default {
+  components: {
+    DynComp1,
+    DynComp2,
+    DynComp3
+  },
+  data() {
+    return {
+      activecomponent: 'DynComp1'
+    }
+  },
+  methods: {
+    setcomp(comp) {
+      this.activecomponent = comp
+    }
+  }
+}
+</script>
+
+<template>
+ 
+
+      <div>
+        <button @click="setcomp('DynComp1')">DynComp1</button>
+        <button @click="setcomp('DynComp2')">DynComp2</button>
+        <button @click="setcomp('DynComp3')">DynComp3</button>
+       
+
+      </div>
+
+      <div>
+        <keep-alive>
+          <component :is="activecomponent"></component>
+        </keep-alive>
+        
+      </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/DynComp2.vue
@@ -0,0 +1,21 @@
+<script>
+export default{
+
+    data(){
+        return{
+            inputdata: ''
+        }
+    },
+}
+</script>
+
+<template>
+  <div>
+    <h2>Dynamic Component 2</h2>
+    <input type="text" v-model="inputdata">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
